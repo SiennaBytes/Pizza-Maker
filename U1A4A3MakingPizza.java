b1f6552a60c24284cef354f81314f8db87004f3f
midnightnymph <106698765+midnightnymph@users.noreply.github.com>
2023-04-12 20:36:00 -0400

    Update U1A4A3MakingPizza.java

--- U1A4A3MakingPizza.java
+++ U1A4A3MakingPizza.java
@@ -1,12 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author Sienna
  */
 public class U1A4A3MakingPizza extends javax.swing.JFrame {
 
